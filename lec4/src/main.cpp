f5783553cb7e6c18c3cf9db2a9d88ae6354e0cec
Abraham <brahimaitelmahjoub12345@gmail.com>
2023-09-01 16:29:11 +0100

    Doooooooooone

--- lec4/src/main.cpp
+++ lec4/src/main.cpp
@@ -4,6 +4,37 @@
 #include <sstream>
 #include <string>
 
+class FontClass
+{
+private:
+    sf::Font font;
+    sf::Color color;
+    int size;
+public:
+    void setFont(const std::string &font_path, int font_size, int R, int G, int B)
+    {
+        if (!font.loadFromFile(font_path))
+        {
+            std::cout << "Cannot load the font" << std::endl;
+            
+        }
+        size = font_size;
+        color = sf::Color(R, G, B);
+        
+    }
+    const sf::Font& getFon(void)
+    {
+        return font;
+    }
+    const sf::Color& getColor(void)
+    {
+        return color;
+    }
+    const int getSize(void)
+    {
+        return size;
+    }
+};
 
 class Shape 
 {
@@ -11,8 +42,9 @@ protected:
     std::string name;
     int  R, G, B;
     float  x, y, sx, sy;
+    sf::Text text;
 public:
-    Shape(const std::string& name ,float x, float y, float sx, float sy, int R, int G, int B)
+    Shape(const std::string& name ,float x, float y, float sx, float sy, int R, int G, int B, FontClass& font)
     : name(name)
     , x(x)
     , y(y)
@@ -22,6 +54,10 @@ public:
     , G(G)
     , B(B)
     {
+        text.setFont(font.getFon());
+        text.setFillColor(font.getColor());
+        text.setString(name);
+        text.setCharacterSize(font.getSize());
     }
 };
 class Rect : public Shape 
@@ -29,9 +65,10 @@ class Rect : public Shape
 private:
     int W, H;
     sf::RectangleShape rect;
+    sf::Vector2f rectangleCenter;
 public:
-    Rect(const std::string& name ,float x, float y, float sx, float sy, int R, int G, int B, float w, float h)
-    : Shape(name, x, y, sx, sy, R, G, B)
+    Rect(const std::string& name ,float x, float y, float sx, float sy, int R, int G, int B, float w, float h, FontClass& font)
+    : Shape(name, x, y, sx, sy, R, G, B, font)
     , W(w)
     , H(h)
     {
@@ -43,9 +80,22 @@ public:
     {
         
         win.draw(rect);
+        win.draw(text);
     }
     void update(int win_width, int win_heigth)
     {
+        sf::Vector2f rectangleCenter = rect.getPosition() + 0.5f * rect.getSize();
+        sf::FloatRect textBounds = text.getLocalBounds();
+        text.setOrigin(textBounds.left + textBounds.width / 2.0f, textBounds.top + textBounds.height / 2.0f);
+        text.setPosition(rectangleCenter);
+        if (rect.getPosition().x < 0 | rect.getPosition().x + rect.getSize().x >= win_width)
+        {
+            sx *= - 1;
+        }
+        else if(rect.getPosition().y < 0 | rect.getPosition().y + rect.getSize().y >= win_heigth)
+        {
+            sy *= - 1;
+        }
         rect.move(sx, sy);
     }
 };
@@ -54,31 +104,41 @@ class Circle : Shape
 {
 private: 
     float radius;
-    sf::CircleShape cirlce;
+    sf::CircleShape circle;
+    sf::Vector2f circleCenter;
+    
 public:
-    Circle(const std::string& name ,float x, float y, float sx, float sy, int R, int G, int B, float raduis)
-    : Shape(name, x, y, sx, sy, R, G, B)
+    Circle(const std::string& name ,float x, float y, float sx, float sy, int R, int G, int B, float raduis, FontClass& font)
+    : Shape(name, x, y, sx, sy, R, G, B, font)
     , radius(radius)
     {
-        cirlce.setRadius(raduis);
-        cirlce.setFillColor(sf::Color(R, G, B));
-        cirlce.setPosition(x, y);
+        circle.setRadius(raduis);
+        circle.setFillColor(sf::Color(R, G, B));
+        circle.setPosition(x, y);
+        
     }
     void draw(sf::RenderWindow & win)
     {
-        win.draw(cirlce);
+        win.draw(circle);
+        win.draw(text);
     }
     void update(int win_width, int win_heigth)
     {
-        if(x <= 0 || x + radius >= win_width)
+        sf::Vector2f CircleCenter = circle.getPosition() + sf::Vector2f(circle.getRadius(), circle.getRadius());
+        sf::FloatRect textBounds = text.getLocalBounds();
+        text.setOrigin(textBounds.left + textBounds.width / 2.0f, textBounds.top + textBounds.height / 2.0f);
+        text.setPosition(CircleCenter);
+        if (circle.getPosition().y < 0 | circle.getPosition().y + 2 * (circle.getRadius()) > win_heigth )
         {
-            std::cout << "do something" << std::endl;
+            
+            sy *= -1;
         }
-        else if (y <= 0 || y + radius >= win_heigth)
+        else if (circle.getPosition().x < 0 | circle.getPosition().x + 2 * ( circle.getRadius()) > win_width)
         {
-            std::cout << "do something again" << std::endl;
+            
+            sx *= - 1;
         }
-        cirlce.move(sf::Vector2f(sx, sy));
+        circle.move(sx, sy);
     }
 };
 
@@ -95,6 +155,7 @@ int main() {
     std::string name;
     float x, y, sx, sy, w, h, radius;
     int R, G, B;
+    FontClass ft;
     std::vector <Circle> cirlces;
     std::vector <Rect> rects;
 
@@ -110,21 +171,26 @@ int main() {
         else if (command == "circle")
         {
             sss >> name >> x >> y >> sx >> sy >> R >> G >> B >> radius;
-            Circle circle(name, x, y, sx, sy, R, G, B, radius);
+            Circle circle(name, x, y, sx, sy, R, G, B, radius, ft);
             cirlces.push_back(circle);
         }
         else if (command == "rectangle")
         {
             sss >> name >> x >> y >> sx >> sy >> R >> G >> B >> w >> h;
-            Rect rect(name, x, y, sx, sy, R, G, B, w, h);
+            Rect rect(name, x, y, sx, sy, R, G, B, w, h, ft);
             rects.push_back(rect);
+        }
+        else if (command == "font")
+        {
+            sss >> name >> x >> R >> G >> B;
+            ft.setFont(name, x, R, G, B);
         }
     }
 
     configFile.close();
    
     // Create an SFML window
-    sf::RenderWindow window(sf::VideoMode(width, height), "SFML Example");
+    sf::RenderWindow window(sf::VideoMode(width, height), "C4003");
     
     while (window.isOpen()) 
     {
